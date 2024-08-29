dfe93869b893f1ec65d70e64c2e866b762a9687d
Matheus <hisamoto.331@gmail.com>
2024-08-29 16:05:17 -0300

    exercicios

--- estudando_CP.py
+++ estudando_CP.py
@@ -1,10 +1,22 @@
 frase = 'Curso em video python'
+print(frase)
 print(len( frase))
 print(frase[9:21])
 print(frase[9:21:2])
 print(frase[:5])
 print(frase[5:])
 print(frase[5::3])
+print(frase[::3])
+print(frase.count("o"))
+frase.find("deo")
+frase.replace('python', 'android')
+print("curso" in frase)
+print(frase)
+frase.split()
+'-'.join(frase)
 
+print(""" 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc arcu urna, tristique non mi id, ultricies rhoncus tortor. Quisque eu turpis dictum, condimentum risus vitae, consectetur ante. Vestibulum mauris arcu, cursus eu vulputate in, tincidunt sit amet mi. Duis consectetur purus quis pretium scelerisque. Nullam euismod mi tellus. Aenean fermentum fringilla tempus. Morbi a nibh mi.
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum ex ex, elementum mattis purus semper, lacinia tempor elit. Suspendisse potenti. Duis sed blandit augue, eu pellentesque metus. Donec dapibus felis at purus tempor elementum. Quisque accumsan massa et varius eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus ut suscipit nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent venenatis viverra mi sit amet aliquam. Pellentesque eu est aliquet, sodales ipsum eu, aliquam tortor. Maecenas aliquet ultrices ante ut condimentum. Fusce fermentum posuere leo id consequat. Morbi vehicula congue risus.""")
 
 
---
+++ exercicios.py
@@ -0,0 +1,18 @@
+'''
+nome = input('Digite seu nome completo: ')
+print(nome.upper())
+print(nome.lower())
+print(len(nome))
+print(len(nome.replace(' ', '')))
+nomesep = nome.split()
+print(len(nomesep[0]))
+'''
+'''
+num = str(input("digite um número de 1000 a 9999: "))
+
+print('Analisando o número {}'.format(num))
+print('unidade: {}'.format(num[3]))
+print('dezena: {}'.format(num[2]))
+print('centena: {}'.format(num[1]))
+print('milhar: {}'.format(num[0]))
+'''
